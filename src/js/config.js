57a00f31ac9877376beed9e4ddb1363c02689df5
hoanglongne <ngytrhoanglong61@gmail.com>
2021-08-07 17:53:51 +0700

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7b51399b-2729-435e-a77d-4f7cb8c8f346'
+
+
+export const MODAL_CLOSE_SEC = 2.5;
